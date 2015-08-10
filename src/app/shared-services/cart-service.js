0a3441fdb4cfdedf023bc0e7cc9ab08b23fc2ceb
Luke Garrison <lgarriso@nd.edu>
2015-08-09 23:40:57 -0400

    Cart-service checks if new item is already in cart

--- src/app/shared-services/cart-service.js
+++ src/app/shared-services/cart-service.js
@@ -6,7 +6,17 @@ cartService.service('CartService', ['EventService', function (EventService) {
 	var totalQuantity = 0;
 
 	this.addItem = function (item) {
-		cart.push(item);
+		var existingCartIndex = getIndex(item);
+		console.log(existingCartIndex);
+		if (existingCartIndex === false) {
+			item.qty = 1;
+			cart.push(item);
+			console.log('    new item');
+		} else {
+			cart[existingCartIndex].qty++;
+			console.log('    existing item');
+		}
+
 		totalQuantity++;
 		total += item.price;
 		EventService.broadcast('item-added');
@@ -23,4 +33,36 @@ cartService.service('CartService', ['EventService', function (EventService) {
 	this.getCart = function () {
 		return cart;
 	};
+
+	// return index of item if it already exists in the cart. If new item is unique, returns false
+	function getIndex(newItem) {
+		if (cart.length === 0) {
+			return false;
+		}
+
+		// cart.forEach(function (item, index) {
+		for (var i = 0; i < cart.length; i++) {
+			var item = cart[i];
+
+			if (newItem.name !== item.name) {
+				continue;
+			} else if (newItem.type !== item.type) {
+				continue;
+			} else if (newItem.price !== item.price) {
+				continue;
+			} else if (newItem.description !== item.description) {
+				continue;
+			} else if (newItem.index !== item.index) {
+				continue;
+			} else if (newItem.option !== item.option) {
+				continue;
+			} else if (_.xor(newItem.ingredients, item.ingredients).length !== 0) {
+				continue;
+			}
+
+			return i;
+		}
+
+		return false;
+	}
 }]);
